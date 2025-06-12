ee60ce41487bbd9c965c24db2cf97948f9a481d8
maxim <bozzhik@ya.ru>
2025-06-12 19:41:08 +0300

    init `requests` file for `sanity` entites

---
+++ src/sanity/lib/requests.ts
@@ -0,0 +1,33 @@
+import {sanityFetch} from '@/sanity/lib/live'
+import {defineQuery} from 'next-sanity'
+import {draftMode} from 'next/headers'
+
+async function fetchEntity<T>(query: string, draft: boolean = true): Promise<T[]> {
+  try {
+    const {isEnabled} = await draftMode()
+
+    const response = await sanityFetch({
+      query,
+      ...(isEnabled && draft
+        ? {
+            perspective: 'drafts',
+            useCdn: false,
+            stega: true,
+          }
+        : undefined),
+    })
+    return (response.data as T[]) || []
+  } catch (error) {
+    console.log('Error fetching data:', error)
+    return []
+  }
+}
+
+const MEDIA_QUERY = defineQuery(`
+    *[_type == "media"]{
+        heading, date, source, size, image,
+    }`)
+
+const QUERIES = {
+  MEDIA_QUERY,
+} as const
